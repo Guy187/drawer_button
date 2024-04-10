cb0f74a65e12cbb6f8b6c1784a44e7e925b1f0dd
guy187 <guylherme699@gmail.com>
2024-04-09 21:47:27 -0300

    Projeto adicionado

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('drawerBtn').addEventListener('click', function() {
+    document.getElementById('drawer').style.left = '0';
+  });
+  
+  document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('drawer').style.left = '-3000px';
+  });
+  
